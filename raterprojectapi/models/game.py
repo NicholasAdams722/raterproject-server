4387f26e5ac10f04fe1c8a78f18b89f633d933fd
Nicholas Adams <njadams.722@gmail.com>
2023-05-17 10:42:52 -0500

    may 15th collab project

--- raterprojectapi/models/game.py
+++ raterprojectapi/models/game.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Game(models.Model):
     title = models.CharField(max_length=55)
     description = models.CharField(max_length=280)
     designer = models.CharField(max_length=55)
-    year_released  = models.IntegerField(validators=[MinValueValidator(1500), MaxLengthValidator(9999)])
+    year_released  = models.IntegerField(validators=[MinValueValidator(1500), MaxValueValidator(9999)])
     number_of_players = models.IntegerField()
     estimated_time = models.DurationField() #“DD HH:MM:SS.uuuuuu”
     age_recommendation = models.IntegerField()
--- raterprojectapi/models/rating.py
+++ raterprojectapi/models/rating.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Rating(models.Model):
-    rating: models.IntegerField()
+    rating: models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     game: models.ForeignKey("Game", on_delete=models.CASCADE)
     player: models.ForeignKey("Player", on_delete=models.CASCADE)
